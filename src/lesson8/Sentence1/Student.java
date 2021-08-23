40b8955adbd2832734a440a5a81b3fd067420a84
tuanta <tuana1k56@gmail.com>
2021-08-23 21:25:46 +0700

    lesson8

---
+++ src/lesson8/Sentence1/Student.java
@@ -0,0 +1,71 @@
+package lesson8.Sentence1;
+
+public class Student {
+    private int studentCode;
+    private String name;
+    private String major;
+    private float theoryScore;
+    private float praticeScore;
+
+    public Student(int studentCode, String name, String major, float theoryScore, float praticeScore) {
+        this.studentCode = studentCode;
+        this.name = name;
+        this.major = major;
+        this.theoryScore = theoryScore;
+        this.praticeScore = praticeScore;
+    }
+
+    public Student() {
+    }
+
+    public double getStudentCode() {
+        return studentCode;
+    }
+
+    public void setStudentCode( int studentCode) {
+        this.studentCode = studentCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+    public float getTheoryScore() {
+        return theoryScore;
+    }
+
+    public void setTheoryScore(float theoryScore) {
+        this.theoryScore = theoryScore;
+    }
+
+    public float getPraticeScore() {
+        return praticeScore;
+    }
+
+    public void setPraticeScore(float praticeScore) {
+        this.praticeScore = praticeScore;
+    }
+    public float calculateAverageScore(float dlt , float dth){
+        dlt = theoryScore;
+        dth = praticeScore;
+        float dtb = (dlt+dth)/2;
+        return dtb;
+    }
+    public String toString(){
+        return studentCode  + name  + major + theoryScore + praticeScore  + calculateAverageScore(theoryScore,praticeScore);
+    }
+    public void inSV() {
+        System.out.printf("%8d %9s %15s %6s %20s %10s  %2f %10s %2f %10s %2f \n", studentCode  ,"",  name ,"", major ,"",theoryScore , "" , praticeScore , "", calculateAverageScore(theoryScore,praticeScore));
+    }
+}
---
+++ src/lesson8/Sentence1/StudentManagement.java
@@ -0,0 +1,41 @@
+package lesson8.Sentence1;
+
+import java.util.Scanner;
+
+public class StudentManagement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Student sv1 = new Student(20052002, "Trần Anh Tuấn", "Công nghệ thông tin", 5, 5);
+        Student sv2 = new Student();
+        Student sv3 = new Student();
+        System.out.println("Enter studentCode 2:");
+        sv2.setStudentCode(sc.nextInt());
+        sc.nextLine();
+        System.out.println("Enter name:");
+        sv2.setName(sc.nextLine());
+        System.out.println("Enter major: ");
+        sv2.setMajor(sc.nextLine());
+        System.out.println("Enter theoryScore:");
+        sv2.setTheoryScore(sc.nextFloat());
+        sc.nextLine();
+        System.out.println("Enter praticeScore:");
+        sv2.setPraticeScore(sc.nextFloat());
+        sc.nextLine();
+        System.out.println("Enter studentCode 3:");
+        sv3.setStudentCode(sc.nextInt());
+        sc.nextLine();
+        System.out.println("Enter name:");
+        sv3.setName(sc.nextLine());
+        System.out.println("Enter major: ");
+        sv3.setMajor(sc.nextLine());
+        System.out.println("Enter theoryScore:");
+        sv3.setTheoryScore(sc.nextFloat());
+        sc.nextLine();
+        System.out.println("Enter praticeScore:");
+        sv3.setPraticeScore(sc.nextFloat());
+        System.out.printf("%8s %2s %15s %4s %20s %6s %20s %5s %10s %5s %10s \n ","Mã sinh viên","","Họ tên","","Ngành","","Điểm lý thuyết" ,"","Điểm thực hành","","Điểm trung bình");
+        sv1.inSV();
+        sv2.inSV();
+        sv3.inSV();
+    }
+}
